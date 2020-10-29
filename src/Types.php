62e728ec9bc5bdcbfea030eaf507cbaebed0f559
Ivaylo Papazov <papazov@vmandco.com>
2020-10-29 15:21:58 +0200

    Remove unsuported transactions

--- src/Types.php
+++ src/Types.php
@@ -5,9 +5,6 @@ namespace Borica;
 class Types
 {
     const PAYMENT = 1;
-    const INITIAL_AUTHORIZATION = 12;
-    const AUTHORIZATION_COMPLETION = 21;
-    const AUTHORIZATION_CANCELLATION = 22;
     const PAYMENT_CANCELLATION = 24;
     const STATUS_TRANSACTION = 90;
 }
